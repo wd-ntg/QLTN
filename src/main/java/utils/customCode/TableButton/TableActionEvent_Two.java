9dc8e9499a52cea22f9e64b4330c926b3716072a
nguyenthanht632 <99719212+nguyenthanht632@users.noreply.github.com>
2024-06-02 20:36:04 +0700

    update table HOPDONG

---
+++ src/main/java/utils/customCode/TableButton/TableActionEvent_Two.java
@@ -0,0 +1,9 @@
+
+package utils.customCode.TableButton;
+
+
+public interface TableActionEvent_One {
+    public void onEdit(int row, int column);
+    public void onDelete(int row,int column);
+    public void onView(int row, int column);
+}
